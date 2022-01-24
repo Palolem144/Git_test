ed725b8974d151b8b85fc31b4a6ef922b44c458a
Zaitsev Egor <palolem144@mail.ru>
2022-01-24 15:24:32 +0300

    Add sort

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-Arr =[]
+#Получить с клавиатуры 6 чисел.
+#Вывести на экран их в возрастающем порядке.
+A_rr = []
 print("Введите 6 целых чисел:")
 for i in range (0, 6):
-b = int(input())
-Arr.append(b)
+    b = int(input())
+    A_rr.append(b)
